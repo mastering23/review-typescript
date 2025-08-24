3984f4bbdfc00b24913edc2c15030d3142f5fbb8
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-08-24 02:17:04 -0400

    adding basic let and const variables

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,19 @@
+const print = console.log;
+
+//---------------------------//
+
+let firstname: string ='Fernado';
+const lastname: string = 'Herrera';
+
+let diceNumber: number = 3;
+
+print(firstname,lastname,diceNumber);
+
+//-----------------------------//
+
+
+const containsLetterH = lastname.includes('H');
+
+print(containsLetterH);
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+import './bases/01-const-let';
 
 
 
