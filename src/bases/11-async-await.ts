1801521d98605f0e685a9e2ebefb088ed06f6cb2
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-08-26 09:48:56 -0400

    async and await handles implementation

--- src/bases/11-async-await.ts
+++ src/bases/11-async-await.ts
@@ -3,22 +3,16 @@ import { print } from "./08-imp-exp";
 
 const API_KEY = 'HWuTUrcEL2FtFGqNQRtN0V5TfqAnNUsI';
 
-const MyRequest  = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
-
 const createImageInsideDom = (url:string) =>{
     const imgElement = document.createElement('img');
     imgElement.src = url; 
     document.body.append(imgElement);
 };
 
-MyRequest.then((response)=>{
-  response.json().then(({data}: GiphyResponseTs)=>{
-  const imageUrl = data.images.original.url;
-  createImageInsideDom(imageUrl);
-  });
+const getRandomGifUrl = async (): Promise <string> =>{
+  const response  = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
+  const  {data} : GiphyResponseTs = await response.json();
+  return data.images.original.url;
+};
 
-}).catch((err)=>{
-  
-  print(err);
-
-});
+getRandomGifUrl().then((createImageInsideDom));
